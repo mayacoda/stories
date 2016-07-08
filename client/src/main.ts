e9c9070e579dfdc0719450adaa3dd9f0c28997bf
Maja Nedeljković <maja.nedeljkovic@sbgenomics.com>
2016-07-08 16:27:33 -0400

    Formatted app files.

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,9 +1,9 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { AppComponent, environment } from './app/';
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {AppComponent, environment} from './app/';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrap(AppComponent);
