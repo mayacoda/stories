aed46b5e1293720d62aa7c2e7e15b99b2cf26c0a
Maja Nedeljković <maya.nedeljko@gmail.com>
2016-07-10 08:01:17 -0400

    Added sauce labs support to travis, attempting to get e2e tests to run on CI.

--- client/config/protractor.conf.js
+++ client/config/protractor.conf.js
@@ -4,29 +4,43 @@
 /*global jasmine */
 var SpecReporter = require('jasmine-spec-reporter');
 
-exports.config = {
-  allScriptsTimeout: 11000,
-  specs: [
-    '../e2e/**/*.e2e-spec.ts'
-  ],
-  capabilities: {
-    'browserName': 'chrome'
-  },
-  directConnect: true,
-  baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
-  },
-  useAllAngular2AppRoots: true,
-  beforeLaunch: function() {
-    require('ts-node').register({
-      project: 'e2e'
-    });
-  },
-  onPrepare: function() {
-    jasmine.getEnv().addReporter(new SpecReporter());
-  }
+var config = {
+    allScriptsTimeout: 11000,
+    specs: [
+        '../e2e/**/*.e2e-spec.ts'
+    ],
+    capabilities: {
+        'browserName': 'chrome'
+    },
+    directConnect: true,
+    baseUrl: 'http://localhost:4200/',
+    framework: 'jasmine',
+    jasmineNodeOpts: {
+        showColors: true,
+        defaultTimeoutInterval: 30000,
+        print: function() {
+        }
+    },
+    useAllAngular2AppRoots: true,
+    beforeLaunch: function() {
+        require('ts-node').register({
+            project: 'e2e'
+        });
+    },
+    onPrepare: function() {
+        jasmine.getEnv().addReporter(new SpecReporter());
+    }
 };
+
+
+if (process.env.TRAVIS) {
+    config.sauceUser = process.env.SAUCE_USERNAME;
+    config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+    config.capabilities = {
+        'browserName': 'chrome',
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        'build': process.env.TRAVIS_BUILD_NUMBER
+    };
+}
+
+exports.config = config;
